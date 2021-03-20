ed5c09e460ef9ebe0f0f18eac2c76c08f7988eb3
iorgadev <2624432+iorgadev@users.noreply.github.com>
2021-03-20 05:23:44 -0400

    board works, moves almost working

---
+++ components/Board.js
@@ -0,0 +1,128 @@
+import { useState, useEffect } from "react";
+import styles from "../styles/Board.module.css";
+// import knight from "../public/knight.svg";
+
+const Board = () => {
+  const [selected, setSelected] = useState(null);
+  const [possibleMoves, setPossibleMoves] = useState([]);
+  const [board, setBoard] = useState([]);
+
+  // const [state, setstate] = useState(initialState)
+  const [player, setPlayer] = useState(5);
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    setBoard(createBoard());
+  }, []);
+
+  useEffect(() => {
+    // console.log(player);
+  }, [board]);
+
+  const createBoard = (playerPos = 0) => {
+    let _board = [];
+    let cur = 0;
+    setPlayer(5);
+
+    for (let file = 0; file < 8; file++) {
+      for (let rank = 0; rank < 8; rank++) {
+        _board[cur++] = square(cur);
+      }
+    }
+
+    return _board;
+  };
+
+  const updateBoard = () => {
+    if (player) {
+      let _board = board;
+      for (let i in board) {
+        // console.log(i);
+      }
+    }
+  };
+
+  let h = [2, 1, -1, -2, -2, -1, 1, 2];
+  let v = [-1, -2, -2, -1, 1, 2, 2, 1];
+  let moves = [-6, -15, -17, -10, 6, 15, 17, 10];
+
+  const clicked = (pos) => {
+    console.log(canMove(pos));
+  };
+
+  const canMove = (pos) => {
+    let moveable = false;
+    let movePos;
+    // console.log(player);
+    let xy = getXY(player);
+    for (let i = 0; i < 8; i++) {
+      movePos = player + moves[i];
+      // newBounds = getXY(pos);
+      if (movePos == pos && validBounds({ x: xy.x + h[i], y: xy.y + v[i] })) {
+        moveable = true;
+      }
+    }
+    return moveable;
+  };
+
+  const drawSquare = (pos) => {
+    return (
+      <div
+        id={pos}
+        key={pos}
+        className={
+          styles.tile +
+          ` ${isSelected(pos) ? styles["selected"] : ` `}
+            ${canMove(pos) ? styles["can-move"] : ` `}
+            ${isLightSquare(pos) ? styles["tile-light"] : styles["tile-dark"]}`
+        }
+        onClick={(e) => clicked(pos)}
+      >
+        {board[pos].active ? (
+          <img className={styles.player} src="./knight.svg" />
+        ) : (
+          <h1 className={styles.num}>{pos}</h1>
+        )}
+      </div>
+    );
+  };
+
+  let square = (pos) => {
+    // console.log(playerPos);
+    // console.log(pos == playerPos + 1 ? pos : "");
+    return {
+      x: getXY(pos).x,
+      y: getXY(pos).y,
+      pos: pos,
+      light: isLightSquare(pos),
+      selected: pos == player,
+      visited: false,
+      active: pos == player + 1,
+      canMove: false,
+    };
+  };
+
+  const getXY = (i) => {
+    return {
+      x: i % 8,
+      y: Math.floor(i / 8),
+    };
+  };
+
+  //needs fix for board object
+  const validBounds = ({ x, y }) => {
+    return x >= 0 && x <= 7 && y >= 0 && y <= 7;
+    // return pos >= 0 && pos <= 63;
+  };
+
+  const isLightSquare = (pos) => getXY(pos).x % 2 == getXY(pos).y % 2;
+  const isSelected = (pos) => board[pos].selected;
+
+  return (
+    <div id="board" className={styles.board}>
+      {board.map((e, pos) => drawSquare(pos))}
+    </div>
+  );
+};
+
+export default Board;
--- pages/index.js
+++ pages/index.js
@@ -1,101 +1,28 @@
-import Head from "next/head";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Board from "../components/Board";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  let white = "#ffffff";
-  let black = "#000000";
-  let currentlySelected = null;
-  let previousSelected = null;
-  let board = [64];
-
-  let square = (x, y, pos, color) => {
-    return {
-      x: x,
-      y: y,
-      pos: pos,
-      light: color,
-      selected: false,
-      visited: false,
-      active: false,
-      canMove: false,
-    };
-  };
-
-  const getXY = (i) => {
-    return {
-      x: i % 8,
-      y: Math.floor(i / 8),
-    };
-  };
-
-  const getBoard = () => {
-    return board;
-  };
-
-  const getIndex = (pos) => {
-    return pos.x + 1 + pos.y * 8;
-  };
-
-  const validBounds = (pos) => {
-    return pos.x >= 0 && pos.x <= 7 && pos.y >= 0 && pos.y <= 7;
-  };
-
-  const isLightSquare = (pos) => getXY(pos).x % 2 == getXY(pos).y % 2;
-
-  const createBoard = () => {
-    let _board = [];
-    let cur = 0;
-
-    for (let file = 0; file < 8; file++) {
-      for (let rank = 0; rank < 8; rank++) {
-        let lightSquare = isLightSquare(cur);
-        _board[cur++] = square(file, rank, cur, lightSquare);
-      }
-    }
-    // board = _board;
-    return _board;
-  };
-
-  const drawSquare = (pos) => {
-    return (
-      <div
-        id={pos}
-        key={pos}
-        className={`w-full box-border border-2 border-yellow-${
-          isLightSquare(pos) ? `200` : `700`
-        } relative pt-[100%] bg-yellow-${isLightSquare(pos) ? `200` : `700`}`}
-        onClick={(e) => clicked(pos)}
-      ></div>
-    );
-  };
-
-  const clicked = (i) => {
-    console.log(board[i]);
-  };
-
-  useEffect(() => {
-    board = createBoard();
-  }, [board]);
-
   return (
     <>
       <div className="flex flex-col items-center justify-center w-full h-screen space-y-2 bg-gray-900">
-        <div className="">
-          <button className="p-3 text-white border">test</button>
+        <div className="flex space-x-4">
+          <button className="p-3 text-white border">Button</button>
+          <button className="p-3 text-white border">Button</button>
+          <button className="p-3 text-white border">Button</button>
         </div>
-        <div className="w-[800px] h-[800px] bg-gray-500">
+        <div className="w-auto h-auto bg-gray-500">
           {/* make the board */}
-          <div className="grid grid-cols-8 gap-0">
-            {createBoard().map((e, pos) => drawSquare(pos))}
-          </div>
+          {/* <div className="grid grid-cols-8 gap-0"> */}
+          <Board />
+          {/* </div> */}
         </div>
       </div>
 
       {/* stats */}
       <div className="w-full h-48 bg-gray-700"></div>
 
-      <div className="box-border bg-yellow-200 bg-yellow-700 border-yellow-200 border-yellow-700 border-blue-700"></div>
+      <div className="box-border w-10 h-10 bg-yellow-200 bg-yellow-700 bg-yellow-900 border-yellow-200 border-yellow-700 border-yellow-900 border-blue-700"></div>
     </>
   );
 }
